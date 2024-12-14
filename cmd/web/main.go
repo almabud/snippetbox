82b012efbdc4d8e5e35393a8d4cd342baa2de22e
almabud <almabud37@gmail.com>
2024-12-15 04:35:00 +0600

    feat: Implement file server to serve static files.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static"
+	// directory. This is relative path.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as
+	// the handler for all URL paths that start with /static/.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)

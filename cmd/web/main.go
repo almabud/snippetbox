b0c8ee8372aede8cac73f34e3321ee59a23589a2
almabud <almabud37@gmail.com>
2024-12-16 17:04:52 +0600

    feat: Decentralize routes.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -42,21 +42,7 @@ func main() {
 		errorLog: errorLog,
 		infoLog:  infoLog,
 	}
-
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static"
-	// directory. This is relative path.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as
-	// the handler for all URL paths that start with /static/.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
+	mux := app.routes()
 	// Initialize a new http.Serveer struct. We set the addr and handler fields
 	// so that the server uses the same network address and routes as before, and ste
 	// the ErrorLog field so that the server now uses the custom errorLog loggeer in
